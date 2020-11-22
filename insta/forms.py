2d17a4f20614683549b7bb0c6cda6800b6f9cfa6
Munge Ryan <mungeryan@gmail.com>
2020-11-22 10:25:26 +0300

    creatin a class update user form

--- insta/forms.py
+++ insta/forms.py
@@ -14,3 +14,10 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('image', 'caption')
+
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
