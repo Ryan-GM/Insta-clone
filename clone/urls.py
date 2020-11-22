ba1970ea7c0c90654365248ac35d22db1ca0f1b7
Munge Ryan <mungeryan@gmail.com>
2020-11-22 14:10:12 +0300

    changing to url from path

--- clone/urls.py
+++ clone/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.conf.urls import url,include
 from django.conf import settings
 from django.conf.urls.static import static 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('insta.urls'))
+    url('admin/', admin.site.urls),
+    url('',include('insta.urls'))
 ]
 
 if settings.DEBUG:
