8f77362fc5008cc915d7abca731b83264d07da93
Munge Ryan <mungeryan@gmail.com>
2020-11-22 10:25:53 +0300

    creatin a class comment form

--- insta/forms.py
+++ insta/forms.py
@@ -21,3 +21,13 @@ class UpdateUserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'email')
+
+class CommentForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['comment'].widget = forms.TextInput()
+        self.fields['comment'].widget.attrs['placeholder'] = 'Add a comment...'
+
+    class Meta:
+        model = Comment
+        fields = ('comment',)
