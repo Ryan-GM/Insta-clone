d96303515c3b02561166f085a28b94da316015db
Munge Ryan <mungeryan@gmail.com>
2020-11-23 13:23:28 +0300

    changing to path

--- insta/urls.py
+++ insta/urls.py
@@ -2,15 +2,16 @@ from django.conf.urls import url
 from django.conf.urls import url,include
 from insta.views import PostLikeAPIToggle,PostLikeToggle
 from . import views
+from django.urls import path
 urlpatterns = [
     url('signup/', views.signup, name='signup'),
     url('account/', include('django.contrib.auth.urls')),
     url('', views.index, name='index'),
-    url('profile/<username>/', views.profile, name='profile'),
-    url('user_profile/<username>/', views.user_profile, name='user_profile'),
-    url('post/<id>', views.post_comment, name='comment'),
-    url('post/<id>/like', PostLikeToggle.as_view(), name='liked'),
-    url('api/post/<id>/like', PostLikeAPIToggle.as_view(), name='liked-api'),
+    path('profile/<username>/', views.profile, name='profile'),
+    path('user_profile/<username>/', views.user_profile, name='user_profile'),
+    path('post/<id>', views.post_comment, name='comment'),
+    path('post/<id>/like', PostLikeToggle.as_view(), name='liked'),
+    path('api/post/<id>/like', PostLikeAPIToggle.as_view(), name='liked-api'),
     url('like', views.like_post, name='like_post'),
     url('search/', views.search_profile, name='search'),
     url('unfollow/<to_unfollow>', views.unfollow, name='unfollow'),
